80701584393e73b9dd1ca98e6d85f72e32ce0a4b
Łukasz Olbrycht <lukasz.olbrycht@gmail.com>
2024-08-03 19:22:44 +0200

    Add captcha

--- src/components/feature/ContactForm/ContactForm.js
+++ src/components/feature/ContactForm/ContactForm.js
@@ -1,16 +1,37 @@
-import { useState } from 'react';
+import { useState, useRef } from 'react';
 import Button from '../../common/Button/Button';
 import styles from './ContactForm.module.scss';
 import emailjs from '@emailjs/browser';
+import ReCAPTCHA from 'react-google-recaptcha';
 
 const ContactForm = () => {
 
     const [name, setName] = useState('');
     const [email, setEmail] = useState('');
     const [msg, setMsg] = useState('');
+    const [status, setStatus] = useState('hide'); // hide, success, fail
+    const [honeypot, setHoneypot] = useState('');
+    const [captchaValue, setCaptchaValue] = useState('');
 
-    const formHandler = (e) => {
+    const recaptchaRef = useRef();
+
+    const handleCaptcha = (value) => {
+        setCaptchaValue(value);
+        console.log("Captcha value:", value);
+    };
+
+    const formHandler = async (e) => {
         e.preventDefault();
+
+        if(!name || !email || !msg || !!captchaValue){
+            console.log('brak danych');
+            return;
+        }
+
+        if (honeypot) {
+            console.log('Bot detected');
+            return; 
+        }
         
         const templateParams = {
             from_name: name,
@@ -18,35 +39,69 @@ const ContactForm = () => {
             reply_to: email
         };
 
-        emailjs.send(
-            process.env.REACT_APP_EMAILJS_SERVICE_ID, 
-            process.env.REACT_APP_EMAILJS_TEMPLATE_ID, 
-            templateParams, 
-            process.env.REACT_APP_EMAILJS_USER_ID
-        )
-        .then((result) => {
+        try{
+            const result = await emailjs.send(
+                process.env.REACT_APP_EMAILJS_SERVICE_ID, 
+                process.env.REACT_APP_EMAILJS_TEMPLATE_ID, 
+                templateParams, 
+                process.env.REACT_APP_EMAILJS_USER_ID
+            );
             console.log('Email sent: ', result.text);
-        }, (error) => {
-            console.error('Error sending email: ', error.text);
-        });
 
+            setName('');
+            setEmail('');
+            setMsg('');
+            setStatus('success');
+        } catch (err) {
+            console.error('Error sending email: ', err.text || err.message);
+            setStatus('fail');
+        }
     };
+
     return(
         <div className={styles.wrapper} onSubmit={formHandler}>
             <p className={styles.title}>Wypełnij formularz</p>
+            { status === 'success' && 
+                <div className={styles.success}>
+                    <p>Twoj email zostal wysłany </p>
+                </div>
+            }
+            { status === 'fail' && 
+                <div className={styles.fail}>
+                    <p> Ups.. Coś poszło nie tak... </p>
+                </div>
+            }
             <form className={styles.form}>
+                <input 
+                    type="text" 
+                    name="honeypot" 
+                    className={styles.honeypot} 
+                    value={honeypot} 
+                    onChange={e => setHoneypot(e.target.value)}
+                    placeholder="Leave this field empty"
+                    
+                />
                 <div className={styles.personalData}>
                     <div className={styles.dataWrapper}>
                         <label htmlFor='name'>Imię</label>
                         <input 
-                            type='text' placeholder='Twoje imię' id='name' name='name' 
+                            type='text' 
+                            placeholder='Twoje imię' 
+                            id='name' 
+                            name='name' 
+                            required
+                            minLength={3}
                             value={name} onChange={e => setName(e.target.value)}
                         />
                     </div>
                     <div className={styles.dataWrapper}>
                         <label htmlFor='email'>E-mail</label>
                         <input 
-                            type='email' placeholder='Zostaw e-mail, żebym mogła do Ciebie napisać!' id='email' name='email'
+                            type='email' 
+                            placeholder='Zostaw e-mail, żebym mogła do Ciebie napisać!' 
+                            id='email' 
+                            name='email'
+                            required
                             value={email} onChange={e => setEmail(e.target.value)}
                         />
                     </div>
@@ -54,8 +109,20 @@ const ContactForm = () => {
                 <div className={styles.msg}>
                     <label htmlFor='msg'> Wiadomość</label>
                     <textarea 
-                        placeholder='Twoja wiadomość' id='msg' name='msg' rows={10}
+                        placeholder='Twoja wiadomość' 
+                        id='msg' 
+                        name='msg' 
+                        rows={10} 
+                        required
+                        minLength={10}
                         value={msg} onChange={e => setMsg(e.target.value)}
+                    />
+                </div>
+                <div className={styles.captcha}>
+                    <ReCAPTCHA
+                        sitekey={process.env.REACT_APP_RECAPTCHA_SITE_KEY} 
+                        onChange={handleCaptcha}
+                        ref={recaptchaRef}
                     />
                 </div>
                 <div className={styles.btnWrapper}>
